c40ba507c8918e0b4ec2ed84a0319c7db504505b
cknox <cknox@bu.edu>
2023-04-20 00:23:45 -0400

    background

--- main.cpp
+++ main.cpp
@@ -12,6 +12,8 @@ int main(int argc, char* argv[]) {
     // Create a container widget to hold the pet widgets
     QWidget* containerWidget = new QWidget();
     containerWidget->setContentsMargins(0, 0, 0, 0);
+    containerWidget->setStyleSheet("background-image: url(:sprites/_background.png); background-size: cover;");
+
     //containerWidget->setMinimumSize(480, 272);
     // Create a grid layout to arrange the pet widgets
     QGridLayout* layout = new QGridLayout(containerWidget);
@@ -25,6 +27,12 @@ int main(int argc, char* argv[]) {
     pets.push_back(Pet("AvoCato","A cross between an avocado and a cat?!?!? the scientific comunity will be in shambles","AvoCato1","AvoCato2",0));
     pets.push_back(Pet("Catcus", "cacteye", "CATcus1", "CATcus2", 0));
     pets.push_back(Pet("the great catsbty", "the great", "Catsby1", "Catsby2", 0));
+    pets.push_back(Pet("CatBall","Ball","CatBall1","CatBall2",0));
+    pets.push_back(Pet("CatBall","Ball","CatBall3","CatBall4",0));
+    pets.push_back(Pet("CatBall","Ball","CatBall5","CatBall6",0));
+    pets.push_back(Pet("CatBall","Ball","CatBall7","CatBall8",0));
+    pets.push_back(Pet("CatBall","Ball","CatBall9","CatBall10",0));
+    pets.push_back(Pet("CatBall","Ball","CatBall11","CatBall12",0));
 
     PetsWidget barn(containerWidget,pets);
     layout->addWidget(&barn);
