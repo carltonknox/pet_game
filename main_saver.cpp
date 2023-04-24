28530e4dd20fe2dc64de5540e8b0d6215ebb20ed
Carlton Knox <cknox@bu.edu>
2023-04-24 19:17:41 -0400

    main_saving

--- main_saver.cpp
+++ main_saver.cpp
@@ -5,7 +5,26 @@
 #include <QGridLayout>
 #include <QMainWindow>
 #include "PetsWidget.hpp"
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <iostream>
 int main(int argc, char* argv[]) {
+    //write own pid into kernel module
+    pid_t pid = getpid();
+    std::cout<<"my pid is "<<pid<<"\n";
+
+    int fd = open("/dev/kscreensaverlauncher",O_WRONLY);
+    if(fd<0){
+        perror("kernel module not loaded to /dev/kscreensaverlauncher. exiting...");
+        return -1;
+    }
+    if(write(fd,&pid,sizeof(pid_t))!=sizeof(pid_t)){
+        perror("pid write failed");
+        return -1;
+    }
+    close(fd);
+    
 
 // Set the random seed based on the current time
     srand(time(NULL));
